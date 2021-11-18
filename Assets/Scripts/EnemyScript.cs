b57b59aaf45ba34cd34e22c2e56592d5f680d254
Elard Burga Goicochea <elard3d@hotmail.com>
2021-11-18 17:16:29 -0500

    Enemy sigue al player

--- Assets/Scripts/EnemyScript.cs
+++ Assets/Scripts/EnemyScript.cs
@@ -1,10 +1,26 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyScript : MonoBehaviour
 {
+    private Transform player;
     [SerializeField] private int health = 1;
+    [SerializeField] private float speed = 1;
+
+    private void Start()
+    {
+         player = FindObjectOfType<PlayerScript>().transform;
+    }
+
+    private void Update()
+    {
+        //obtener direccion hacia el jugador
+        Vector2 direction = player.position - transform.position;
+        transform.position += (Vector3)direction * Time.deltaTime * speed;
+
+    }
 
     public void TakeDamage()
     {
