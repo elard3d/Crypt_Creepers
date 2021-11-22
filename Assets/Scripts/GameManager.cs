e3be52e88e07732fbf9224478e9e0f4cfd34b264
Elard Burga Goicochea <elard3d@hotmail.com>
2021-11-22 12:12:48 -0500

    GameManager CREADO, CountDown paa GAMEOVER FUNCIONANDO

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] private int time = 30;
+    
+    //Patron de Diseño Singleton
+    public static GameManager Instance;
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this; //retorna toda la info de GameMAnager
+        }
+    }
+
+    private void Start()
+    {
+        StartCoroutine(CountdownRoutine());
+    }
+
+    IEnumerator CountdownRoutine()
+    {
+        while (time>0)
+        {
+            yield return new WaitForSeconds(1);
+            time--;
+        }
+        
+        //GAMEOVER
+        
+        Debug.Log("GAMEOVER");
+    }
+}
